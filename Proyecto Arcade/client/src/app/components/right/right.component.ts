7498c7c821f4bce78e4d63c219ebf5f2955dfba6
KobosilDev <kobosilwow@gmail.com>
2023-12-15 21:39:55 +0100

    añadir y bajar stock

--- Proyecto Arcade/client/src/app/components/right/right.component.ts
+++ Proyecto Arcade/client/src/app/components/right/right.component.ts
@@ -35,5 +35,16 @@ export class RightComponent {
     this.productos[1].nameProduct = "Producto 2";
     this.productos[1].imageProduct = "https://pm1.aminoapps.com/7706/0369015221a1b48e3e6b0440906e82d1abc9dd49r1-430-512v2_uhq.jpg";
     this.productos[1].category_id = "655ba587ce5c90942dd4c9a1";
+    
+    this.productos.push(new Cart());
+    this.productos[2]._id = "";
+    this.productos[2]._idProduct = "";
+    this.productos[2]._idClient = "";
+    this.productos[2].description = "Muy guapo";
+    this.productos[2].price = 333;
+    this.productos[2].amount = 3;
+    this.productos[2].nameProduct = "Producto 2";
+    this.productos[2].imageProduct = "https://pm1.aminoapps.com/7706/0369015221a1b48e3e6b0440906e82d1abc9dd49r1-430-512v2_uhq.jpg";
+    this.productos[2].category_id = "655ba587ce5c90942dd4c9a1";
   }
 }
