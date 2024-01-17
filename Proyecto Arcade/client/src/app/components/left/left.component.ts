328feaa64d9c9315f37ac90c9aa5d556287054c7
KobosilDev <kobosilwow@gmail.com>
2024-01-17 20:10:48 +0100

    Comienzo cambiar componente categories

--- Proyecto Arcade/client/src/app/components/left/left.component.ts
+++ Proyecto Arcade/client/src/app/components/left/left.component.ts
@@ -1,38 +1,16 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Category } from 'src/app/models/category';
-import { CategoryService } from 'src/app/services/category.service';
+import { CategoriesComponent } from '../categories/categories.component';
 
 @Component({
   selector: 'app-left',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, CategoriesComponent],
   templateUrl: './left.component.html',
   styleUrls: ['./left.component.css']
 })
 
 export class LeftComponent {
 
-  @Output() categoriaEnviada = new EventEmitter<string>();
-
-  constructor(public categoryService: CategoryService){
-    
-  }
-
-  PasarCategoria(_id: any){
-    this.categoriaEnviada.emit(_id);
-  }
-
-  obtaingCategories() {
-    this.categoryService.showCategories()
-    .subscribe(res => {
-      this.categoryService.categories = res as Category[];
-      console.log(res);
-    })
-  }
-
-  ngOnInit(): void {
-    this.obtaingCategories();
-  }
+  
 }
