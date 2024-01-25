3429a802b425f5a2a256000a168b95c6b7e71a4f
Mari0BG <81181738+Mari0BG@users.noreply.github.com>
2024-01-25 22:12:04 +0100

    h

--- Proyecto Arcade/api/routes/user.js
+++ Proyecto Arcade/api/routes/user.js
@@ -5,7 +5,7 @@ import { verifyAdmin, verifyUser } from '../utils/verifyToken.js'
 const router = express.Router()
 
 //getall
-router.get('/', getAllUsers )   // se verifica el token y solo puede entrar usuario admin
+router.get('/getAll', getAllUsers )   // se verifica el token y solo puede entrar usuario admin
 
 //get by id
 router.get('/:id', getById )  // se verifica que el usuario logado es el unico que puede acceder a su perfil
